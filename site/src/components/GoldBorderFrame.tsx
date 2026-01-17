cc2a516aae544865aae0d23fabc529716f3f7e38
Ray <rayndaula@gmail.com>
2026-01-17 07:42:12 -0500

    Add glowing leaf borders with gradient background and sway animations

--- site/src/components/GoldBorderFrame.tsx
+++ site/src/components/GoldBorderFrame.tsx
@@ -1,21 +1,100 @@
+import Image from "next/image";
+
 export default function GoldBorderFrame() {
     return (
         <>
-            {/* Outer gold border frame */}
-            <div className="gold-border-frame" aria-hidden="true" />
+            {/* Left leaf border */}
+            <div className="leaf-border leaf-border-left" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
+            </div>
 
-            {/* Corner decorations */}
-            <div className="fixed top-6 left-6 text-[var(--color-gold)] text-4xl z-10 pointer-events-none" aria-hidden="true">
-                ❧
+            {/* Right leaf border */}
+            <div className="leaf-border leaf-border-right" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={60}
+                    height={400}
+                    className="leaf-image"
+                    priority
+                />
             </div>
-            <div className="fixed top-6 right-6 text-[var(--color-gold)] text-4xl z-10 pointer-events-none scale-x-[-1]" aria-hidden="true">
-                ❧
+
+            {/* Corner leaf accents */}
+            <div className="leaf-corner leaf-corner-tl" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={80}
+                    height={150}
+                    className="leaf-corner-image"
+                />
             </div>
-            <div className="fixed bottom-6 left-6 text-[var(--color-gold)] text-4xl z-10 pointer-events-none scale-y-[-1]" aria-hidden="true">
-                ❧
+            <div className="leaf-corner leaf-corner-tr" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={80}
+                    height={150}
+                    className="leaf-corner-image"
+                />
             </div>
-            <div className="fixed bottom-6 right-6 text-[var(--color-gold)] text-4xl z-10 pointer-events-none scale-[-1]" aria-hidden="true">
-                ❧
+            <div className="leaf-corner leaf-corner-bl" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={80}
+                    height={150}
+                    className="leaf-corner-image"
+                />
+            </div>
+            <div className="leaf-corner leaf-corner-br" aria-hidden="true">
+                <Image
+                    src="/leaf-border.png"
+                    alt=""
+                    width={80}
+                    height={150}
+                    className="leaf-corner-image"
+                />
             </div>
         </>
     );
